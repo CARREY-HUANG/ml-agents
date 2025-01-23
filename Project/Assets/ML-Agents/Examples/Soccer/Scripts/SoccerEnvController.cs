f2d28846a0f58b59d075eabe14ad0befeb730202
Zhengzhong Carrey HUANG <972173378@qq.com>
2025-01-23 14:12:43 +0100

    fix soccer env

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerEnvController.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerEnvController.cs
@@ -113,22 +113,7 @@ public class SoccerEnvController : MonoBehaviour
         ResetScene();
 
     }
-    public void OnGoalScored(Team scoringTeam)
-    {
-        foreach (var agent in agents)
-        {
-            var agentSoccer = agent.GetComponent<AgentSoccer>();
-            if (agentSoccer.team == scoringTeam)
-            {
-                agentSoccer.RewardForGoal(false); // Reward for scoring a goal
-            }
-            else
-            {
-                agentSoccer.RewardForGoal(true); // Penalty for conceding a goal
-            }
-        }
-        ResetField(); // Reset the field after a goal is scored
-    }
+
 
 
 
